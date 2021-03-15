eba51a60f96427f9d7f2e9f5fc345d1ec9c7af66
Bhavik Parmar <bhavik2936@gmail.com>
2021-03-15 14:08:54 +0530

    unique email constraint added
    duplicate email cannot be inserted for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_many :wishes
 
+  before_validation :downcase_email
+
   validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end
