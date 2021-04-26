86159b5826c073fe843940cfe771d8241ce46320
Bhavik Parmar <bhavik2936@gmail.com>
2021-04-26 19:31:05 +0530

    tweaked environment.rb
    removed division of class fieldWithErrors which wraps the error containing element in erb file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Removal of division wrapping with fieldWithErrors class
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
